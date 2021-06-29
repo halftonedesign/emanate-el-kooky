1f8daf4b1f4d82e536d0288c63732a0cdf601e03
Keeley Nakamoto <keeleynakamoto@Keeleys-MBP.attlocal.net>
2021-06-29 11:30:50 -0700

    Add function to calculate max profit

--- problems/buy-and-sell-stock-once/solution.js
+++ problems/buy-and-sell-stock-once/solution.js
@@ -1,1 +1,11 @@
-// your code here
+function buy_and_sell_stock_once(prices){
+    var min_final = prices[0];
+    var max_final = 0;
+    prices.forEach(price => {
+        var max_temp = price - min_final < 0 ? price - min_final : price;
+        max_final = Math.max(max_final, max_temp);
+        min_final = Math.min(min_final,price);
+    })
+    return max_final;
+  }
+  
